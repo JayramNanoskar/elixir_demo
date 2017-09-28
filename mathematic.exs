51e437bd58613a971749937d3aa55dd800f02f65
JayramNanoskar <supriya.medankar@kiprosh.com>
2017-09-28 19:33:12 +0530

    math function

---
+++ mathematic.exs
@@ -0,0 +1,9 @@
+defmodule Mathematics do
+def prod(a,b) do
+a*b
+end
+end
+IO.puts "Hi...welcome in elixir programming";
+IO.puts Mathematics.prod(22,35);
+IO.puts Math.sum(22,35);
+IO.puts Math.minus(35,11);
